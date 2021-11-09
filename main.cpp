a15111dd658a249f3b18085b310c06166a0eb0e2
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-09 14:01:34 +0300

    plan is ready

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 void number_of_products_div() {
-    cout << "Hello world!" << endl;
+    /**
+    1. считать число
+    2. увеличить один из счётчиков: % 15, % 5, % 3, не %
+    3. ответ: n15 * (n15 - 1) + n15 * n5 + n15 * n3 + n15 * n0 + n5 * n3
+    */
 }
 
 int main()
