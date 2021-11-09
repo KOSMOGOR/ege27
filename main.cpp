cdc7cba327a17b6305b0c5cbb6f0bb9b3f3b3e57
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-09 14:03:21 +0300

    read numbers

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 void number_of_products_div() {
     /**
-    1. считать число
+    + 1. считать число
     2. увеличить один из счётчиков: % 15, % 5, % 3, не %
     3. ответ: n15 * (n15 - 1) + n15 * n5 + n15 * n3 + n15 * n0 + n5 * n3
     */
+    int n, cur;
+    cin >> n;
+    for (int i(0); i < n; ++i) {
+        cin >> cur;
+        cout << cur;
+    }
 }
 
 int main()
