4e28d886582e8dd7781cbe7b39b6e43d6036d4a9
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-16 12:38:22 +0300

    print all pairs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int control_sum() {
     /**
     + 1. ввести и сохранить числа
-    2. вывести все пары
+    + 2. вывести все пары
     3. вместо пра вывести суммы
     4. проверить делимость сумм
     5. вывести только наибольшую
@@ -14,8 +14,11 @@ int control_sum() {
     int n;
     cin >> n;
     vector<int> numbers(n);
-    for (int(i); i < n; ++i)
+    for (int i(0); i < n; ++i)
         cin >> numbers[i];
+    for (int i(0); i < n - 1; ++i)
+        for (int j(0); j < n; ++j)
+            cout << numbers[i] << " " << numbers[j] << endl;
 }
 
 int main()
