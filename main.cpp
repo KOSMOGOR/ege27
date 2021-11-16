b66512fbf1c6f207459c08ca18223381b6c7da55
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-16 14:01:30 +0300

    check answer

--- main.cpp
+++ main.cpp
@@ -5,20 +5,21 @@ using namespace std;
 int control_sum() {
     /**
     + 1. считывать только наибольшее наиболььшее чётное и нечётное число
-    2. проверить, есть ли решение
+    + 2. проверить, есть ли решение
     3. вывести ответ
     */
     int n, num;
     cin >> n;
-    int maxe(-1001), mino(-1001);
+    int maxe(-1001), maxo(-1001);
     for (int i(0); i < n; ++i) {
         cin >> num;
         if (num % 2) {
-            if (num > mino) mino = num;
+            if (num > maxo) maxo = num;
         } else {
             if (num > maxe) maxe = num;
         }
     }
+    if (maxe == 0 || maxo == 0) cout << -1;
 }
 
 int main()
