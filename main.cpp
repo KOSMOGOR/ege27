f2430cbcae23c5f5dbc77a24976fe5fa9cd1a6d7
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-16 12:43:04 +0300

    print only max

--- main.cpp
+++ main.cpp
@@ -9,19 +9,20 @@ int control_sum() {
     + 2. вывести все пары
     + 3. вместо пар вывести суммы
     + 4. проверить делимость сумм
-    5. вывести только наибольшую
+    + 5. вывести только наибольшую
     */
     int n;
     cin >> n;
     vector<int> numbers(n);
     for (int i(0); i < n; ++i)
         cin >> numbers[i];
+    int maxx(-2000);
     for (int i(0); i < n - 1; ++i)
         for (int j(0); j < n; ++j) {
             int s = numbers[i] + numbers[j];
-            if (s % 2)
-                cout << s << endl;
+            if (s % 2 && s > maxx) maxx = s;
         }
+    cout << (s == -2000 ? -1 : s);
 }
 
 int main()
