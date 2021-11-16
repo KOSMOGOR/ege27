6b475f46eafd4813e0aee9beac3d27fa26ea04a2
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-16 12:41:33 +0300

    check division

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int control_sum() {
     + 1. ввести и сохранить числа
     + 2. вывести все пары
     + 3. вместо пар вывести суммы
-    4. проверить делимость сумм
+    + 4. проверить делимость сумм
     5. вывести только наибольшую
     */
     int n;
@@ -17,8 +17,11 @@ int control_sum() {
     for (int i(0); i < n; ++i)
         cin >> numbers[i];
     for (int i(0); i < n - 1; ++i)
-        for (int j(0); j < n; ++j)
-            cout << numbers[i] + numbers[j] << endl;
+        for (int j(0); j < n; ++j) {
+            int s = numbers[i] + numbers[j];
+            if (s % 2)
+                cout << s << endl;
+        }
 }
 
 int main()
