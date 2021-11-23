f1615953cb7a257641fad7de54e84025d7a2bb91
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-23 12:56:03 +0300

    print pairs with needed product

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ void number_of_products_div() {
     + 1. считать и запомнить входные числа
     + 2. вывести все пары
     + 3. вместо вывода пары посчитать её произведение
-    4. вывести только пары, произведение которых кратно 55
-    5. кол-во пар, произведение которых кратно 15
+    + 4. вывести только пары, произведение которых кратно 385
+    5. кол-во пар, произведение которых кратно 385
     */
     int n;
     cin >> n;
@@ -17,8 +17,9 @@ void number_of_products_div() {
     for (int i(0); i < n; ++i)
         cin >> numbers[i];
     for (int i(0); i < n - 1; ++i)
-        for (int j(i); j < n; ++j)
-            cout << numbers[i] * numbers[j] << endl;
+        for (int j(i); j < n; ++j) {
+            if ((numbers[i] * numbers[j]) % 385 == 0)
+                cout << numbers[i] * numbers[j] << endl;
 }
 
 int main()
