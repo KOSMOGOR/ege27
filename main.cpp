c3825962b7b2fc82fd9b197a785a54f894974454
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-23 12:54:04 +0300

    print all pairs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void number_of_products_div() {
     /**
     + 1. считать и запомнить входные числа
-    2. вывести все пары
+    + 2. вывести все пары
     3. вместо вывода пары посчитать её произведение
     4. вывести только пары, произведение которых кратно 55
     5. кол-во пар, произведение которых кратно 15
@@ -16,6 +16,9 @@ void number_of_products_div() {
     vector<int> numbers(n, 0);
     for (int i(0); i < n; ++i)
         cin >> numbers[i];
+    for (int i(0); i < n - 1; ++i)
+        for (int j(i); j < n; ++j)
+            cout << numbers[i] << " " << numbers[j] << endl;
 }
 
 int main()
