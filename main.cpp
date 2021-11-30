5d98085aab2ce1ef47f7897cef747b006168babd
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-30 12:39:40 +0300

    print all numbers

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 void min_sum_distance() {
     /**
-    1. считать все числа
-    2. вывести все числа
+    + 1. считать все числа
+    + 2. вывести все числа
     3. вывести все пары
     4. вывести только достаточно далёкие пары
     5. вместо вывода - сумма
@@ -16,6 +16,8 @@ void min_sum_distance() {
     vector<int> numbers(n, 0);
     for (int i(0); i < n; ++i)
         cin >> numbers[i];
+    for (int i(0); i < n; ++i)
+        cout << numbers[i] << endl;
 }
 
 int main()
