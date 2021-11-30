de095506fb93413345df2c7b82030bd08deb061e
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-30 12:40:35 +0300

    print all pairs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ void min_sum_distance() {
     /**
     + 1. считать все числа
     + 2. вывести все числа
-    3. вывести все пары
+    + 3. вывести все пары
     4. вывести только достаточно далёкие пары
     5. вместо вывода - сумма
     6. найти минимальную сумму
@@ -16,8 +16,9 @@ void min_sum_distance() {
     vector<int> numbers(n, 0);
     for (int i(0); i < n; ++i)
         cin >> numbers[i];
-    for (int i(0); i < n; ++i)
-        cout << numbers[i] << endl;
+    for (int i(0); i < n - 1; ++i)
+        for (int j(0); j < n; ++j)
+            cout << numbers[i] << " " << numbers[j] << endl;
 }
 
 int main()
