106118e71a3e305c1e9cf88059b64b359c55f82d
KOSMOGOR <mortalmortal2015@mail.ru>
2021-11-30 12:41:11 +0300

    print all unique pairs

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ void min_sum_distance() {
     for (int i(0); i < n; ++i)
         cin >> numbers[i];
     for (int i(0); i < n - 1; ++i)
-        for (int j(0); j < n; ++j)
+        for (int j(i + 1); j < n; ++j)
             cout << numbers[i] << " " << numbers[j] << endl;
 }
 
