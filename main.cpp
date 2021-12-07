3d8f6c958ef738cd3701afc2828d455075fdf9cb
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-07 12:05:38 +0300

    read numbers

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void min_distance_sum_div() {
     /**
-    1. ввести числа
+    + 1. ввести числа
     2. вывести все возможные пары
     3. вывести пары, разность индексов которых меньше 8
     4. вывести сумму чисел
     5. вывести суммы только кратные 28
     6. вывести количество сумм
     */
+    int n;
+    cin >> n;
+    vector<int> vec(0, n);
+    for (int i(0); i < n; ++i) {
+        cin >> vec[i];
+    }
 }
 
 int main()
