500321b754523a28fe2ceb3bf9ce36401f7bb6e9
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-07 12:45:24 +0300

    print all pairs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void min_distance_sum_div() {
     /**
     + 1. ввести числа
-    2. вывести все возможные пары
+    + 2. вывести все возможные пары
     3. вывести пары, разность индексов которых меньше 8
     4. вывести сумму чисел
     5. вывести суммы только кратные 28
@@ -18,6 +18,9 @@ void min_distance_sum_div() {
     for (int i(0); i < n; ++i) {
         cin >> vec[i];
     }
+    for (int i(0); i < n; ++i)
+        for (int j(0); j < n; ++j)
+            cout << vec[i] << " " << vec[j] << endl;
 }
 
 int main()
