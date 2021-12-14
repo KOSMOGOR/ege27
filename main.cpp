dae0972adc47c6c7622342072a2b59d3534a0afa
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-14 13:04:40 +0300

    read numbers

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void div_distance_max_sum() {
     /**
-    1. ввести числа
+    + 1. ввести числа
     2. вывести все пары
     3. вывести пары с кратной разностью индексов
     4. вывести их сумму
     5. найти максимальную сумму
     */
+    int n;
+    cin >> n;
+    vector<int> vec(0, n);
+    for (int i(0); i < n; ++i)
+        cin >> vec[i];
 }
 
 int main()
