9f5b062a3ca95cb27e71ca3bdb2fed6de522638c
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-14 13:11:37 +0300

    print max sum

--- main.cpp
+++ main.cpp
@@ -9,20 +9,27 @@ void div_distance_max_sum() {
     + 2. вывести все пары
     + 3. вывести пары с кратной разностью индексов
     + 4. вывести их сумму
-    5. найти максимальную сумму
+    + 5. найти максимальную сумму
     */
     int n;
     cin >> n;
+    if (n < 7) {
+        cout << "FAIL";
+        return;
+    }
     vector<int> vec(0, n);
     for (int i(0); i < n; ++i)
         cin >> vec[i];
+    int maxs(vec[0] + vec[6]);
     for (int i(0); i < n - 1; ++i)
         for (int j(i + 1); j < n; ++j) {
             if ((j - i) % 6 == 0) {
-                int s = vec[i] + vec[j];
-                cout << s;
+                int s(vec[i] + vec[j]);
+                if (s > maxs)
+                    maxs = s;
             }
         }
+    cout << maxs;
 }
 
 int main()
