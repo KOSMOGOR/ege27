82acc9617bc981e07ebf49628d45b41426e7082d
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-14 13:05:47 +0300

    print all pairs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void div_distance_max_sum() {
     /**
     + 1. ввести числа
-    2. вывести все пары
+    + 2. вывести все пары
     3. вывести пары с кратной разностью индексов
     4. вывести их сумму
     5. найти максимальную сумму
@@ -16,6 +16,9 @@ void div_distance_max_sum() {
     vector<int> vec(0, n);
     for (int i(0); i < n; ++i)
         cin >> vec[i];
+    for (int i(0); i < n - 1; ++i)
+        for (int j(i); j < n; ++j)
+            cout << vec[i] << " " << vec[j];
 }
 
 int main()
