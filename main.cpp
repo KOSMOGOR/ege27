93e0fb8bf9fcb15c1304a934dd82c6ed21dbcd99
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-21 13:51:28 +0300

    change answer to 3 numbers

--- main.cpp
+++ main.cpp
@@ -18,14 +18,19 @@ void triples_mul_min_div() {
     for (int i(0); i < N; ++i)
         cin >> vec[i];
     int min_p = 0;
+    int min_arr[3];
     for (int i(0); i < N - 2; ++i)
         for (int j(i + 1); j < N - 1; ++j)
             for (int k(j + 1); k < N; ++k) {
                 int p = vec[i] * vec[j] * vec[k];
-                if (p % 4 == 0 && (p < min_p || min_p == 0))
+                if (p % 4 == 0 && (p < min_p || min_p == 0)) {
                     min_p = p;
+                    min_arr[0] = vec[i];
+                    min_arr[1] = vec[j];
+                    min_arr[2] = vec[k];
+                }
             }
-    cout << min_p;
+    cout << min_arr[0] << " " << min_arr[1] << " " << min_arr[2];
 }
 
 int main()
