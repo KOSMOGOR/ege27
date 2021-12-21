95e87ac2101c5e861852049782eca038f01f6210
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-21 12:35:40 +0300

    read numbers

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void triples_mul_min_div() {
     /**
-    1. ввести числа
+    + 1. ввести числа
     2. вывод пар
     3. вывод троек
     4. вывод произведений
     5. вывод произведений, дел€щихс€ на 4
     6. вывод минимального произведени€
     */
+    int N;
+    cin >> N;
+    vector<int> vec(N, 0);
+    for (int i(0); i < N; ++i)
+        cin >> vec[i];
 }
 
 int main()
