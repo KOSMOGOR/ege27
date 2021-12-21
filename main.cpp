307908d9a14cae2d0c3bf0bb9d0877c5c6a09d0e
KOSMOGOR <mortalmortal2015@mail.ru>
2021-12-21 12:36:43 +0300

    print pairs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void triples_mul_min_div() {
     /**
     + 1. ввести числа
-    2. вывод пар
+    + 2. вывод пар
     3. вывод троек
     4. вывод произведений
     5. вывод произведений, делящихся на 4
@@ -17,6 +17,9 @@ void triples_mul_min_div() {
     vector<int> vec(N, 0);
     for (int i(0); i < N; ++i)
         cin >> vec[i];
+    for (int i(0); i < N - 1; ++i)
+        for (int j(i + 1); j < N; ++j)
+            cout << vec[i] << " " << vec[j];
 }
 
 int main()
